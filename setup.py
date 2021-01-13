84c539aa6606fd2762de9d2db279fdeaffdad263
Jonitan <skiba8150@gmail.com>
2021-01-13 17:21:36 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jonitan-chat",
+    version="0.1.0",
+    author="Yonatan Naisteter",
+    author_email="skiba8150@gmail.com",
+    description="Simple calculator implementation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jonitan/chat",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
