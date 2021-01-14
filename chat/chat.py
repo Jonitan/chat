52b00ae95df2f5d58a635128bf90680774dc44e2
Jonitan <skiba8150@gmail.com>
2021-01-14 23:29:36 +0200

    Version 0.1.2 Changed to TCP. WIP

--- chat/chat.py
+++ chat/chat.py
@@ -11,7 +11,6 @@ Options:
 """
 import socket
 import asyncio
-import asyncio_dgram
 
 from docopt import docopt
 from aioconsole import ainput
@@ -20,42 +19,46 @@ from aioconsole import ainput
 def chat():
     arguments = docopt(__doc__)
     _validate_arguments(arguments['<remote-host>'], arguments['<local-port>'], arguments['<remote-port>'])
+
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.gather(_handle_session_receive_task((arguments['<remote-host>'],
-                                                                         arguments['<local-port>'])),
-                                           _handle_session_transmit_task((arguments['<remote-host>'],
-                                                                          arguments['<remote-port>']),
-                                                                         arguments['--username'])))
+    receive_task = asyncio.Task(asyncio.start_server(_handle_session_receive_task, arguments['<remote-host>'],
+                                                     int(arguments['<local-port>'])))
+    transmit_task = asyncio.Task(_handle_session_transmit_task(arguments['<remote-host>'],
+                                                               int(arguments['<remote-port>']),
+                                                               arguments['--username']))
+    loop.run_forever()
 
 
 def _validate_arguments(remote_host: str, local_port: str, remote_port: str):
-    try:
-        if not 0 < int(local_port) < 65536 or not 0 < int(remote_port) < 65536:
-            raise ValueError("Illegal port number")
-        socket.inet_aton(remote_host)
-    except ValueError as e:
-        raise e
-    except socket.error as e:
-        raise e
+    if not 0 < int(local_port) < 65536 or not 0 < int(remote_port) < 65536:
+        raise ValueError("Illegal port number")
+    socket.inet_aton(remote_host)
 
 
-async def _handle_session_receive_task(local_host: tuple):
-    stream = await asyncio_dgram.bind(local_host)
-    while True:
-        incoming_message, _ = await stream.recv()
-        print(incoming_message.decode())
-
-
-async def _handle_session_transmit_task(remote_host: tuple, username: str):
-    stream = await asyncio_dgram.connect(remote_host)
+async def _handle_session_receive_task(reader, _):
     while True:
         try:
-            outgoing_message = (username + ": ").encode() + (await ainput()).encode()
-            await stream.send(outgoing_message)
-        except OSError:
-            print("Message wasn't sent successfully")
-            stream.close()
-            stream = await asyncio_dgram.connect(remote_host)
+            incoming_message = await reader.read(2048)
+            print(incoming_message.decode())
+        except ConnectionResetError as e:
+            raise e
+
+
+async def _handle_session_transmit_task(remote_host: str, remote_port: int, username: str):
+    while True:
+        try:
+            _, writer = await asyncio.open_connection(remote_host, remote_port)
+            break
+        except ConnectionRefusedError:
+            pass
+
+    while True:
+        try:
+            outgoing_message = f'{username}: {await ainput()}'
+            writer.write(outgoing_message.encode())
+        except Exception as e:
+            writer.close()
+            print(e)
 
 
 if __name__ == '__main__':
