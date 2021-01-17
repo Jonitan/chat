643e9aefef860edfbf7007dbf23fc4779cfdf6b1
Jonitan <skiba8150@gmail.com>
2021-01-17 20:06:36 +0200

    Version 0.2.1 Changed to select + threading instead of asyncio.

--- chat/chat.py
+++ chat/chat.py
@@ -10,45 +10,72 @@ Options:
     -h, --help                      Show this message and exit.
 """
 import socket
-from select import select
+import threading
+import select
 from sys import stdin
 from queue import Queue
 
 from docopt import docopt
 
+stop_session = False
 
 def chat():
     arguments = docopt(__doc__)
     _validate_arguments(arguments['<remote-host>'], arguments['<local-port>'], arguments['<remote-port>'])
+
+    outgoing_messages_queue = Queue(maxsize=100)
+
     sock = _connect_to_peer(arguments['<remote-host>'], int(arguments['<local-port>']), int(arguments['<remote-port>']))
 
-    inputs = [sock, stdin]
-    outputs = [sock]
+    session_thread = threading.Thread(target=_handle_session, args=(sock, outgoing_messages_queue,
+                                                                    arguments['--username']))
+    user_input_thread = threading.Thread(target=_handle_user_input, args=(outgoing_messages_queue,))
+    user_input_thread.daemon = True
 
-    outgoing_messages = Queue(maxsize=100)
+    session_thread.start()
+    user_input_thread.start()
 
-    readable, writable, exceptional = select(inputs, outputs, inputs)
+
+def _handle_session(sock, outgoing_messages_queue: Queue, username: str):
     while True:
-        for s in readable:
-            if s == sock:
-                incoming_message = s.recv(2048)
-                if incoming_message:
-                    print(incoming_message)
-                else:
-                    sock.close()
-                    break
-            else:
-                user_input = s.readline()
-                outgoing_messages.put(user_input)
+        readable, writable, exceptional = select.select([sock], [sock], [])
 
-        for s in writable:
-            if not Queue.empty():
-                s.send(outgoing_messages.get(block=False))
-
-        for s in exceptional:
-            if s == sock:
-                sock.close()
+        if readable:
+            if not _handle_session_read(sock):
                 break
+
+        if writable:
+            if not _handle_session_write(sock, outgoing_messages_queue, username):
+                break
+
+
+def _handle_session_read(sock) -> bool:
+    try:
+        incoming_message = sock.recv(1024)
+        if incoming_message:
+            print(incoming_message.decode())
+            return True
+        else:
+            return _shutdown_session(sock)
+    except ConnectionResetError:
+        return _shutdown_session(sock)
+
+
+def _handle_session_write(sock, outgoing_messages_queue: Queue, username: str) -> bool:
+    try:
+        if not outgoing_messages_queue.empty():
+            message = outgoing_messages_queue.get(block=False)
+            sock.sendall(f'{username}: {message}'.encode())
+        return True
+    except ConnectionResetError:
+        return _shutdown_session(sock)
+
+
+def _handle_user_input(outgoing_messages_queue: Queue):
+    while True:
+        for line in stdin:
+            if line:
+                outgoing_messages_queue.put(line)
 
 
 def _connect_to_peer(remote_host: str, local_port: int, remote_port: int):
@@ -60,23 +87,30 @@ def _connect_to_peer(remote_host: str, local_port: int, remote_port: int):
 
 def _connect_as_client(remote_host: str, remote_port: int):
     try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((remote_host, remote_port))
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((remote_host, remote_port))
     except ConnectionRefusedError:
         return None
 
     print(f"Connected as client to: {remote_host}:{remote_port}")
-    return s
+    return sock
 
 
 def _connect_as_server(remote_host: str, local_port: int):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((remote_host, local_port))
-        s.listen()
-        conn, addr = s.accept()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((remote_host, local_port))
+    sock.listen()
+    conn, addr = sock.accept()
+    sock.close()
 
     print(f"Connected as server to: {addr[0]}:{addr[1]}")
     return conn
+
+
+def _shutdown_session(sock) -> bool:
+    print("Disconnected: Peer closed connection.")
+    sock.close()
+    return False
 
 
 def _validate_arguments(remote_host: str, local_port: str, remote_port: str):
